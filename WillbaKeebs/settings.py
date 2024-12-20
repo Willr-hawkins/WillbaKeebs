adcd91892633c7339c52b2524ad520161b19f849
William Hawkins <Hawkinswill02@gmail.com>
2024-12-20 17:18:25 +0000

    Added Spinning title animation to home page

--- WillbaKeebs/settings.py
+++ WillbaKeebs/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
 ALLOWED_HOSTS = ['willbakeebs-e8f640fbaa81.herokuapp.com', '8000-willrhawkin-willbakeebs-t08p1e3tghs.ws-eu117.gitpod.io']
 
 
--- about/views.py
+++ about/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 def home(request):
 
+    return render(request, 'about/home.html')
+
+
+def about(request):
+
     return render(request, 'about/about.html')
